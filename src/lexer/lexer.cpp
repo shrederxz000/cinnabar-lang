67e7e91e91015cd353d77230afec9fe736d0807d
shrederxz <shrederxz000@gmail.com>
2026-02-09 03:04:10 +0300

    вроде форматинг завершен

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -255,97 +255,109 @@ std::vector<token::Token>Lexer::tokenize(std::string_view filepath,std::string_v
     while (current_ch_ != '\0' && cursor_ < code_length_)  {
 
         switch (current_ch_) {
-            case '=':
+            case '=': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::EQ,2));}
                 if (char_at(1) == '>') {tokens.push_back(emit(token::TokenKind::IMPL_ARR,2));}
                 else {tokens.push_back(emit(token::TokenKind::ASSIGN,1));}break;
+            }
 
-            case '+':
+            case '+': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::PLUS_ASS,2));}
                 else {tokens.push_back(emit(token::TokenKind::PLUS,1));}break;
+            }
                     
-            case '-':
+            case '-': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::MINUS_ASS,2));}
                 if (char_at(1) == '>') {tokens.push_back(emit(token::TokenKind::RETURN_ARR,2));}
                 else {tokens.push_back(emit(token::TokenKind::MINUS,1));}break;
+            }
 
-            case '/':
+            case '/': {
                 if (char_at(1) == '*' || char_at(1) == '/') {skip_comments();}
                 else if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::SLASH_ASS,2));}
                 else {tokens.push_back(emit(token::TokenKind::SLASH,1));}break;
+            }
 
-            case '*':
+            case '*': {
                 if (char_at(1) == '*' && char_at(2) == '=') {tokens.push_back(emit(token::TokenKind::POW_ASS,3));}
                 else if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::STAR_ASS,2));}
                 else if (char_at(1) == '*') {tokens.push_back(emit(token::TokenKind::POW,2));}
                 else {tokens.push_back(emit(token::TokenKind::STAR,1));}break;
+            }
   
-            case '&':
+            case '&': {
                 if (char_at(1) == '&') {tokens.push_back(emit(token::TokenKind::AND,2));}
                 else {tokens.push_back(emit(token::TokenKind::AMPERSAND,1));}break;
+            }
                     
-            case '%':
+            case '%': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::PERCENT_ASS,2));}
                 else {tokens.push_back(emit(token::TokenKind::PERCENT,1));}break;
+            }
                     
                     
-            case '|':
+            case '|': {
                 if (char_at(1) == '|') {tokens.push_back(emit(token::TokenKind::OR,2));}
                 else {tokens.push_back(emit(token::TokenKind::PIPE,1));}break;
+            }
                     
                     
-            case ':':
+            case ':': {
                 if (char_at(1) == ':') {tokens.push_back(emit(token::TokenKind::DCOLON,2));}
                 else {tokens.push_back(emit(token::TokenKind::COLON,1));}break;
+            }
                     
                     
-            case '.':
+            case '.': {
                 if (char_at(1) == '.' && char_at(2) == '.') {tokens.push_back(emit(token::TokenKind::ELLIPSIS,3));}
                 else if (char_at(1) == '.') {tokens.push_back(emit(token::TokenKind::RANGE,2));}
                 else {tokens.push_back(emit(token::TokenKind::DOT,1));}break;
+            }
                     
-            case '>':
+            case '>': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::GE,2));}
                 else if (char_at(1) == '>') {tokens.push_back(emit(token::TokenKind::SHIFT_RIGHT,2));}
                 else {tokens.push_back(emit(token::TokenKind::GT,1));}break;
+            }
                     
-            case '<':
+            case '<': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::LE,2));}
                 else if (char_at(1) == '<') {tokens.push_back(emit(token::TokenKind::SHIFT_LEFT,2));}
                 else {tokens.push_back(emit(token::TokenKind::LT,1));}break;
+            }
                     
-            case '!':
+            case '!': {
                 if (char_at(1) == '=') {tokens.push_back(emit(token::TokenKind::NEQ,2));}
                 else {tokens.push_back(emit(token::TokenKind::BANG,1));}break;
+            }
                     
-            case ';':tokens.push_back(emit(token::TokenKind::SEMICOLON,1));break;
-            case '#':tokens.push_back(emit(token::TokenKind::HASH,1));break;
-            case '$':tokens.push_back(emit(token::TokenKind::DOLLAR,1));break;
-            case '(':tokens.push_back(emit(token::TokenKind::RPAR,1));break;
-            case ')':tokens.push_back(emit(token::TokenKind::LPAR,1));break;
-            case '[':tokens.push_back(emit(token::TokenKind::LBRACKET,1));break;
-            case ']':tokens.push_back(emit(token::TokenKind::RBRACKET,1));break;
-            case '{':tokens.push_back(emit(token::TokenKind::LBRACE,1));break;
-            case '}':tokens.push_back(emit(token::TokenKind::RBRACE,1));break;
-            case ',':tokens.push_back(emit(token::TokenKind::COMMA,1));break;
-            case '\'':tokens.push_back(scan_char());break;
-            case '"':tokens.push_back(scan_string());break;
-            case '@':tokens.push_back(emit(token::TokenKind::AT,1));break;
-            case '~':tokens.push_back(emit(token::TokenKind::TILDE,1));break;
-            case '?':tokens.push_back(emit(token::TokenKind::QUESTION,1));break;
-            case '^':tokens.push_back(emit(token::TokenKind::CARET,1));break;
+            case ';': {tokens.push_back(emit(token::TokenKind::SEMICOLON,1));break;}
+            case '#': {tokens.push_back(emit(token::TokenKind::HASH,1));break;}
+            case '$': {tokens.push_back(emit(token::TokenKind::DOLLAR,1));break;}
+            case '(': {tokens.push_back(emit(token::TokenKind::RPAR,1));break;}
+            case ')': {tokens.push_back(emit(token::TokenKind::LPAR,1));break;}
+            case '[': {tokens.push_back(emit(token::TokenKind::LBRACKET,1));break;}
+            case ']': {tokens.push_back(emit(token::TokenKind::RBRACKET,1));break;}
+            case '{': {tokens.push_back(emit(token::TokenKind::LBRACE,1));break;}
+            case '}': {tokens.push_back(emit(token::TokenKind::RBRACE,1));break;}
+            case ',': {tokens.push_back(emit(token::TokenKind::COMMA,1));break;}
+            case '\'': {tokens.push_back(scan_char());break;}
+            case '"':{tokens.push_back(scan_string());break;}
+            case '@': {tokens.push_back(emit(token::TokenKind::AT,1));break;}
+            case '~': {tokens.push_back(emit(token::TokenKind::TILDE,1));break;}
+            case '?': {tokens.push_back(emit(token::TokenKind::QUESTION,1));break;}
+            case '^': {tokens.push_back(emit(token::TokenKind::CARET,1));break;}
 
-            default:
-                if(isspace(current_ch_)) {
-                    skip_whitespace();
-                } else if(isalpha(current_ch_) || current_ch_ == '_') {
-                    tokens.push_back(scan_id_or_keyword());
-                } else if (isdigit(current_ch_)) {
-                    tokens.push_back(scan_number());
-                } else {
-                    throw std::runtime_error("error: unexpected syntax");
-                }
+            default: {
+
+                if(isspace(current_ch_)) {skip_whitespace();} 
+                else if(isalpha(current_ch_) || current_ch_ == '_') {tokens.push_back(scan_id_or_keyword());} 
+                else if (isdigit(current_ch_)) {tokens.push_back(scan_number());} 
+                else {throw std::runtime_error("error: unexpected syntax");}
+            }
+                
         }// switch (current_ch_)
+
     }// while (current_ch_ != '\0' && cursor_ < code_length_)
 
     emit(token::TokenKind::Eof);
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -10,34 +10,39 @@ Parser::Parser(const std::vector<token::Token>& tokens)
     : tokens_(tokens) {}
 
 const token::Token& Parser::peek(size_t offset) const {
-    if (pos_ + offset >= tokens_.size())
+
+    if (pos_ + offset >= tokens_.size()){
         return tokens_.back();
+    }
+
     return tokens_[pos_ + offset];
 }
 
-const token::Token& Parser::advance() {
-    return tokens_[pos_++];// возвращаем следующий токен из списка + сдвигаем pos на 1
-    // ВОПРОС:интересно, здесь созраняется позиция в полях или нет?
-}
+const token::Token& Parser::advance() {return tokens_[pos_++];}
 
 bool Parser::match(token::TokenKind kind) {
-    if (peek().kind() == kind) { // если текущий kind токена соответствует ожидаемому, сдвинемся
-        advance(); // двигаемся вперед 
+
+    if (peek().kind() == kind) {
+        advance();
         return true;
     }
+
     return false;
 }
 
 void Parser::expect(token::TokenKind kind, const char* msg) {
-    if (!match(kind)) // если match возвращает false (то-есть заявленный токен не совпадает с текущим), то выбросим ошибку
+
+    if (!match(kind)){
         throw std::runtime_error(msg);
+    }
+
 }
 
 // MAIN FUNCTION
 std::unique_ptr<ast::Program> Parser::parse_program() {
-    auto prog = std::make_unique<ast::Program>(peek().pos());// создаем корневой pointer
+    auto prog = std::make_unique<ast::Program>(peek().pos());
 
-    while (peek().kind() != token::TokenKind::Eof) { // если kind текущего токена не конец программы, то в тело парсим утверждения и добавляем в тело
+    while (peek().kind() != token::TokenKind::Eof) {
         prog->body.push_back(parse_statement());
     }
 
@@ -46,19 +51,19 @@ std::unique_ptr<ast::Program> Parser::parse_program() {
 
 //'{' <stmt> '}' 
 std::unique_ptr<ast::Block> Parser::parse_block() {
-    auto block = std::make_unique<ast::Block>(peek().pos());// создаем block pointer 
-    expect(token::TokenKind::LBRACE, "expected '{'");// проверка на наличие '{'
+    auto block = std::make_unique<ast::Block>(peek().pos());
+    expect(token::TokenKind::LBRACE, "expected '{'");
 
-    while (peek().kind() != token::TokenKind::RBRACE) { // пока токен не '}' парсим утверждения и записываем в тело блока
+    while (peek().kind() != token::TokenKind::RBRACE) {
         block->statements.push_back(parse_statement());
     }
 
-    expect(token::TokenKind::RBRACE, "expected '}'");// проверяем на наличие '}' (а тут вообще ради безопасности)
-    return block; // возвращаем этот pointer
+    expect(token::TokenKind::RBRACE, "expected '}'");
+    return block;
 }
 
 std::unique_ptr<ast::Node> Parser::parse_statement() {
-    switch (peek().kind()) { // проверяем какой текущий токен и применяем парсинг для конкретного токена
+    switch (peek().kind()) {
         case token::TokenKind::LET:
         case token::TokenKind::CONST:
             return parse_let();
@@ -66,121 +71,120 @@ std::unique_ptr<ast::Node> Parser::parse_statement() {
         case token::TokenKind::RETURN:
             return parse_return();
 
-        default: // а по умолчанию парсим общие утверждения
+        default:
             return parse_expr_stmt();
     }
 }
 
-// 'let'/'const'<ID>'='<expr>';' 
+
 std::unique_ptr<ast::Node> Parser::parse_let() {
-    bool is_const = match(token::TokenKind::CONST); // тут проверка на const
-    if (!is_const) advance(); // если проверка не сработала и is_const остался false значит это - let и двигаемся вперед
+    bool is_const = match(token::TokenKind::CONST);
+    if (!is_const) advance();
 
-    auto name = advance(); // записываем токен и именем и двигаемся вперед
-    expect(token::TokenKind::ID, "expected identifier"); // проверка на наличие имени
+    auto name = advance();
+    expect(token::TokenKind::ID, "expected identifier");
 
-    expect(token::TokenKind::ASSIGN, "expected '='"); // проверка на наличие оператора присвоения
+    expect(token::TokenKind::ASSIGN, "expected '='");
 
-    auto value = parse_expression(); // парсим выражение и записываем как значение
-    expect(token::TokenKind::SEMICOLON, "expected ';'"); // проверка на ';'
+    auto value = parse_expression();
+    expect(token::TokenKind::SEMICOLON, "expected ';'");
 
     return std::make_unique<ast::LetStmt>(
-        name.as<std::string>(), // имя переменной как str
-        is_const, // флаг константности
-        std::move(value), // скопируем значение
-        name.pos() // возврат в родительский struct pos
-        // еще должен сказать, что kind уже определен в дочерней LetStmt
-    ); // вернем LetStmt с прарметрами выше
+        name.as<std::string>(),
+        is_const,
+        std::move(value),
+        name.pos()
+        
+    );
 }
 
 // 'return'<expr>';' 
 std::unique_ptr<ast::Node> Parser::parse_return() {
-    auto kw = advance(); // записываем return
-    auto value = parse_expression();// сканируем выражение и записываем в значение 
-    expect(token::TokenKind::SEMICOLON, "expected ';'"); // проверка на ;
+    auto kw = advance();
+    auto value = parse_expression();
+    expect(token::TokenKind::SEMICOLON, "expected ';'");
 
     return std::make_unique<ast::ReturnStmt>(
         std::move(value),
         kw.pos()
-    ); // вернем скопированное значение и позицию родительской структуре
+    );
 }
 
 std::unique_ptr<ast::Node> Parser::parse_expr_stmt() {
-    auto expr = parse_expression(); // парсим выражения и записываем
-    expect(token::TokenKind::SEMICOLON, "expected ';'"); // проверка на ';'
-    return expr;// возвращаем
+    auto expr = parse_expression();
+    expect(token::TokenKind::SEMICOLON, "expected ';'");
+    return expr;
 }
 
 std::tuple<int, int> Parser::precedence(token::TokenKind kind) const {
-    switch (kind) { // тут сила каждого оператора и возвращаем приоритет
-        case token::TokenKind::POW:return {30, 0}; // [0]: сила, [1]: 0 - левоассоциативный, 1 - правоассоциативный
-        case token::TokenKind::STAR:
-        case token::TokenKind::SLASH: return {20, 1};
-        case token::TokenKind::PLUS:
-        case token::TokenKind::MINUS: return {10, 1};
-        case token::TokenKind::EQ:
-        case token::TokenKind::NEQ: return {5, 1};
-        default: return {0, 1};
+    switch (kind) {
+        case token::TokenKind::POW: {return {30, 0};} // [0]: сила, [1]: 0 - левоассоциативный, 1 - правоассоциативный
+        case token::TokenKind::STAR: {return {20, 1};}
+        case token::TokenKind::SLASH: {return {20, 1};}
+        case token::TokenKind::PLUS: {return {10, 1};}
+        case token::TokenKind::MINUS: {return {10, 1};}
+        case token::TokenKind::EQ: {return {5, 1};}
+        case token::TokenKind::NEQ: {return {5, 1};}
+        default: {return {0, 1};}
     }
 }
 
 std::unique_ptr<ast::Node> Parser::parse_prefix() {
-    const auto& tok = advance(); // записываем токен + продвигаемся
+    const auto& tok = advance();
 
-    switch (tok.kind()) { // проверяем токен
-        /* если литерал, то создаем pointer Literal
-             * со значением этого литерала и позицию в
-             * родительскую структуру */
-        case token::TokenKind::INT_LITERAL:
-        case token::TokenKind::FLOAT_LITERAL:
-        case token::TokenKind::STRING_LITERAL:
-        case token::TokenKind::CHAR_LITERAL:
+    switch (tok.kind()) {
+        case token::TokenKind::INT_LITERAL: {return std::make_unique<ast::Literal>(tok.value(), tok.pos());}
+        case token::TokenKind::FLOAT_LITERAL: {return std::make_unique<ast::Literal>(tok.value(), tok.pos());}
+        case token::TokenKind::STRING_LITERAL: {return std::make_unique<ast::Literal>(tok.value(), tok.pos());}
+        case token::TokenKind::CHAR_LITERAL: {
             return std::make_unique<ast::Literal>(tok.value(), tok.pos());
             /*FIXME: IDE clion показывает ошибку:
              * "In template: no matching constructor for initialization of 'cxz::ast::Literal'"*/
+        }
 
-
-
-
-        case token::TokenKind::ID: // если id, то создаем pointer ID с именем id и позицией
+        case token::TokenKind::ID: {
             return std::make_unique<ast::Identifier>(
                 tok.as<std::string>(),
                 tok.pos()
             );
-
-        case token::TokenKind::LPAR: { // если токен ( 
-            auto expr = parse_expression(); // парсим выражения
-            expect(token::TokenKind::RPAR, "expected ')'"); // проверка на закрытие )
-            return expr; // возврат выражения
         }
 
-        default: // ну а тут выбрасываем
+        case token::TokenKind::LPAR: {
+            auto expr = parse_expression();
+            expect(token::TokenKind::RPAR, "expected ')'");
+            return expr;
+        }
+
+        default: {
             throw std::runtime_error("unexpected token in expression");
+        }
     }
 }
 
  
-std::unique_ptr<ast::Node> Parser::parse_expression(int min_prec) { // по умолчанию 0
-    auto left = parse_prefix();// парсим префикс и записываем в левый операнд
+std::unique_ptr<ast::Node> Parser::parse_expression(int min_prec) {
+    auto left = parse_prefix();
 
-    while (true) { // создаем цикл. если что, то pattern matching остановит цикл
-        auto kind = peek().kind(); // берем kind токена (это оператор, так как parse_perfix уже продвинул вепред)
-        auto [prec, prec_side] = precedence(kind);// берем силу оператора и сторону ассоциации
-        if (prec < min_prec) break; // если сила меньше заявленной, то сбрасываем цикл
+    while (true) { 
+        auto kind = peek().kind(); 
+        auto [prec, prec_side] = precedence(kind);
 
-        auto op_tok = advance(); // записываем опертор и продвигаемяс вперед
-        auto right = parse_expression(prec + prec_side); // рекурсивно проходимся, каждый раз повышая минимальную силу исходя от текущего токена и записываем подвыражение в правый операнд 
+        if (prec < min_prec) {break;}
 
+        auto op_tok = advance();
+        auto right = parse_expression(prec + prec_side);
         ast::BinaryOp op;
+
         switch (op_tok.kind()) { // проверка токена и запись а как оператор в ast
-            case token::TokenKind::PLUS: op = ast::BinaryOp::ADD; break;
-            case token::TokenKind::MINUS: op = ast::BinaryOp::SUB; break;
-            case token::TokenKind::STAR: op = ast::BinaryOp::MUL; break;
-            case token::TokenKind::SLASH: op = ast::BinaryOp::DIV; break;
-            case token::TokenKind::POW: op = ast::BinaryOp::POW; break;
+            case token::TokenKind::PLUS: {op = ast::BinaryOp::ADD; break;}
+            case token::TokenKind::MINUS: {op = ast::BinaryOp::SUB; break;}
+            case token::TokenKind::STAR: {op = ast::BinaryOp::MUL; break;}
+            case token::TokenKind::SLASH: {op = ast::BinaryOp::DIV; break;}
+            case token::TokenKind::POW: {op = ast::BinaryOp::POW; break;}
             
-            default:
+            default: {
                 throw std::runtime_error("unknown binary operator");
+            }
         }
 
         left = std::make_unique<ast::BinaryExpr>(
